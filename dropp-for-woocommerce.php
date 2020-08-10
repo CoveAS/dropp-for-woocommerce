77242ada3e77864e26bdcd17770c55ccbdb091e7
Eivin Landa <eivin@cove.no>
2020-08-10 18:23:11 +0200

    Bumped the version number

--- dropp-for-woocommerce.php
+++ dropp-for-woocommerce.php
@@ -6,7 +6,7 @@
  * Author:               Cove AS
  * Author URI:           https://cove.no/dropp
  *
- * Version:              1.3.6
+ * Version:              1.3.7
  * Requires at least:    5.2
  * Tested up to:         5.5
  *

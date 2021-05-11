a3fe8499cf6a2cbfad7685b943ad9024c7cd38ca
Eivin Landa <eivin@cove.no>
2021-05-11 17:18:00 +0200

    Bumped the version number

--- classes/class-dropp.php
+++ classes/class-dropp.php
@@ -12,7 +12,7 @@ namespace Dropp;
  */
 class Dropp {
 
-	const VERSION = '1.4.0';
+	const VERSION = '1.4.1';
 
 	/**
 	 * Setup
--- dropp-for-woocommerce.php
+++ dropp-for-woocommerce.php
@@ -6,7 +6,7 @@
  * Author:               Cove AS
  * Author URI:           https://cove.no/dropp
  *
- * Version:              1.4.0
+ * Version:              1.4.1
  * Requires at least:    5.2
  * Tested up to:         5.7
  *

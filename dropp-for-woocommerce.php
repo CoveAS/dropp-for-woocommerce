6105f59745f85148538049be676d55ea2b91adc1
Eivin Landa <eivin@cove.no>
2021-09-03 10:36:00 +0200

    Updated the version number to 1.4.6 and updated the changelog

--- dropp-for-woocommerce.php
+++ dropp-for-woocommerce.php
@@ -6,7 +6,7 @@
  * Author:               Cove AS
  * Author URI:           https://cove.no/dropp
  *
- * Version:              1.4.5
+ * Version:              1.4.6
  * Requires at least:    5.2
  * Tested up to:         5.7
  *
